a34c404566475d4f44e8ca15e1b3866909b06dee
Vasiliev-Leonid <114797981+Vasiliev-Leonid@users.noreply.github.com>
2022-11-10 18:01:21 +0300

    HomeWork4

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в 
+//натуральную степень B (Math.Pow НЕ использовать)
+//Console.WriteLine("введите число A");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("введите число B");
+//int b = Convert.ToInt32(Console.ReadLine());
+//void ToPowDegree(int a, int b)
+//{
+//    int result = 1;
+//    for (int i = 1; i <= b; i++)
+//    {
+//        result = result * a;
+//    }
+//    Console.WriteLine(result);
+//}
+//ToPowDegree(a,b);
+
+//Задача 27: Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//Console.WriteLine("Input integer number");
+//int i = Convert.ToInt32(Console.ReadLine());
+//void MySum(int i)
+//{
+//int sum = 0;
+//    while (i > 0)
+//    {
+//    int step = i % 10;
+//    i = i / 10;
+//    sum = sum + step;
+//    }
+//Console.WriteLine("The Sum of all digits is: " + sum);
+//}
+//MySum(i);
+
+//Задача 29:Напишите программу, которая создает массив заданного 
+//пользователем размера, заполняет массив элементами от 1 до 99 и 
+//выводит их на экран.
+
+//int [] NewArray (int n)
+//{
+//    int [] arr = new int[n];
+//    for (int i = 0; i < n; i++)
+//    {
+//        arr[i] = new Random().Next(0,100);
+//    }
+//    return arr;
+//}
+//void PrintMyArray(int [] arr)
+//{
+//    for (int i = 0; i < arr.Length; i++)
+//        {
+//        Console.Write($"{arr[i]}, ");
+//
+//        }
+//}
+//Console.Write("Input integer number: ");
+//int value = Convert.ToInt32(Console.ReadLine());
+//PrintMyArray(NewArray(value)); 
+
+
